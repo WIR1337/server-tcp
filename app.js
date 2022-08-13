b0b9583eb71c0e410cdec861100e6e88f02c4e6e
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-08-13 16:45:42 +0300

    Сообщения видят все пользователи

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const net = require('net');
 
-const server = net.createServer(Socket => {
-    Socket.write('Hello World !')
-})
+let socketsArr = []
+const server = net.createServer( socket => {
+    socketsArr.push(socket)
 
+    socket.write('Hello im server')
+    socket.on('connection', () => {
+        console.log('New user join server')
+    })
+    socket.on('data', data => {
+        // for (i = 0; i < socketsArr.length; ++i){
+        //     // if (socketsArr[i] == socket)
+        //     //     continue;
+
+        //     socketsArr[i].write(data)
+        // }
+        socketsArr.forEach((user, i) => {
+            user.write(`User: ${data}`)
+        })
+    })
+})
 server.listen(6000)
+
+
